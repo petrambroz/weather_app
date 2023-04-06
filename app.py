a09f2236af843f7b08816489ace6c27a307abb9d
petrambroz <petrambroz@icloud.com>
2023-04-06 12:32:55 +0200

    added comments, full pylint compliance

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
+"""
+provides function for a basic webapp displaying weather in a selected city
+"""
+from os import getenv
 from flask import Flask, render_template, request
 import requests
 from dotenv import load_dotenv
-from os import getenv
 load_dotenv()
 app = Flask(__name__)
 
 
 class Weather():
-    city = "a"
+    """
+    displayed weather values for city. more to be added
+    """
+    city = ""
     temp = 0
     description = ""
     wind = 0
@@ -15,6 +21,10 @@ class Weather():
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
+    """
+    index page, is default set to display weather in Prague.
+    if POST is used, it loads city name from the form and displays that instead.
+    """
     const_url = "https://api.openweathermap.org/data/2.5/weather?"
     url_args = "units=metric&lang=cz"
     if request.method == 'GET':
@@ -24,7 +34,8 @@ def index():
         city = request.form['city']
     response = requests.post(const_url+url_args+"&q="
                              + city+"&appid="
-                             + getenv("API_KEY"))
+                             + getenv("API_KEY"),
+                             timeout=10)
     if response.status_code != 200:
         return "Město nenalezeno."
     weather_data = response.json()
